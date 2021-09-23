aa8367cad9292fed265fe63bd34247ae022149a7
Tanner <tanner.creque@gmail.com>
2021-09-23 15:05:48 -0600

    formatting + packages

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
-import logo from './logo.svg';
 import './App.css';
+
+
 
 function App() {
   return (
     <div className="App">
+      <h1>My name is Tanner Creque</h1>
+      <h3>and I am a Full-Stack Software Engineer</h3>
     </div>
   );
 }
--- src/Component/Projects.js
+++ src/Component/Projects.js
@@ -1,11 +1,9 @@
 import React from 'react'
 
-function Card() {
+export const Projects = () => {
     return (
         <div>
             
         </div>
     )
 }
-
-export default Card
