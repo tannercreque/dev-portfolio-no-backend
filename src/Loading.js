cbb385dee1d0818a98667bddfbe8af6eabe0a3df
Tanner <tanner.creque@gmail.com>
2021-09-23 14:40:01 -0600

    loader fix + heirarchy

--- src/Loading.js
+++ src/Loading.js
@@ -1,25 +1,14 @@
-import { useState } from "react";
-import { css } from "@emotion/react";
-import ClipLoader from "react-spinners/ClipLoader";
-
-const override = css`
-  display: block;
-  margin: 0 auto;
-  border-color: red;
-`;
-
-function Loading() {
-  let [loading, setLoading] = useState(true);
-  let [color, setColor] = useState("#ffffff");
-
-  return (
-    <div className="sweet-loading">
-      <button onClick={() => setLoading(!loading)}>Toggle Loader</button>
-      <input value={color} onChange={(input) => setColor(input.target.value)} placeholder="Color of the loader" />
-
-      <ClipLoader color={color} loading={loading} css={override} size={150} />
-    </div>
-  );
+import Loader from "react-loader-spinner";
+export default class Loading extends React.Component {
+  render() {
+    return (
+      <Loader
+        type="Puff"
+        color="#00BFFF"
+        height={100}
+        width={100}
+        timeout={3000} //3 secs
+      />
+    );
+  }
 }
-
-export default Loading;
