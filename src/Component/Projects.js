401533e6a99f6f9ecfa37dcc4e28e25d011afca3
Tanner <tanner.creque@gmail.com>
2021-09-24 00:25:55 -0600

    fetch call moved to Project.js

--- src/Component/Projects.js
+++ src/Component/Projects.js
@@ -6,6 +6,14 @@ state = {
     description = {}
 }
 
+componentDidMount(); {
+    fetch('http://localhost:3000/projects')
+        .then(response => response.json())
+        .then(response => console.log(response.json))
+        //.then(data => this.setState({ project: data.total }))   
+  }
+
+  
 export const Projects = () => {
     return (
         <div>
--- src/Container/ProjectsContainer.js
+++ src/Container/ProjectsContainer.js
@@ -1,14 +1,4 @@
 import React from 'react'
-
-
-componentDidMount(); {
-    fetch('http://localhost:3000/projects')
-        .then(response => response.json())
-        .then(data => console.log(data) )
-        //.then(data => this.setState({ project: data.total }))   
-  }
-
-  
   
 export const ProjectsContainer = () => {
     return (
